572b0af12d163bf3abd6c98a9976d5a68973acaa
Edvaldo Ide <edvaldoide@gmail.com>
2025-03-14 14:00:25 -0300

    Chamando a tela de login na classe Main

--- src/main/java/uc15/pi_pdvcongelados/PI_PDVCongelados.java
+++ src/main/java/uc15/pi_pdvcongelados/PI_PDVCongelados.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package uc15.pi_pdvcongelados;
 
-/**
- *
- * @author edval
- */
-public class PI_PDVCongelados {
+import uc15.pi_pdvcongelados.gui.Login;
 
+public class PI_PDVCongelados {
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Login lg = new Login();
+        lg.setVisible(true);
+        
     }
 }
