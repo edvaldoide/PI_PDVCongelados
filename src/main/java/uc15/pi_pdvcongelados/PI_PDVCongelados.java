ccca71311e2d2ebf50573fe8e46245ea708e448a
Edvaldo Ide <edvaldoide@gmail.com>
2025-03-20 23:59:46 -0300

    Elaboração das Telas do Projeto

--- src/main/java/uc15/pi_pdvcongelados/PI_PDVCongelados.java
+++ src/main/java/uc15/pi_pdvcongelados/PI_PDVCongelados.java
@@ -1,12 +1,12 @@
 package uc15.pi_pdvcongelados;
 
-import uc15.pi_pdvcongelados.gui.Login;
+import uc15.pi_pdvcongelados.gui.TelaLogin;
 
 public class PI_PDVCongelados {
     
     public static void main(String[] args) {
         
-        Login lg = new Login();
+        TelaLogin lg = new TelaLogin();
         lg.setVisible(true);
         
     }
